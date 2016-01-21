a2d2a7768b4d5573f6ebe378c0ede5f617cd4d8a
Myles Enriquez <enri.myles@gmail.com>
2016-01-21 11:26:04 -1000

    Rename file to execute in.

--- _plugins/_tag_gen.rb
+++ _plugins/_tag_gen.rb
@@ -8,7 +8,7 @@ module Jekyll
       @dir = dir
       @name = 'index.html'
       self.process(@name)
-      self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
+      self.read_yaml(File.join(base, '_layouts'), 'tag.html')
       self.data['tag'] = tag
     end
   end
