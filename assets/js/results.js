34f8c7b805dc65496f93042231cde078b796bf82
enriquezm <enri.myles@gmail.com>
2016-02-24 10:06:46 -1000

    Lowercase all categories. Add backslash.

--- assets/js/results.js
+++ assets/js/results.js
@@ -84,13 +84,13 @@ $(function(Query) {
 				date = result.date;
 
 				if (category === "How-to") {
-					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details how-to"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + "/" + '" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
+					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details how-to"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
 				}
 				else if (category === 'Strategies') {
-					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details strategies"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category + '" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
+					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details strategies"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
 				}
 				else {
-					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details troubleshooting"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category + '" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
+					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details troubleshooting"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
 				}
 			});
 		}
