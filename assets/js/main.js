d5d2028e14b3d0369c88fa70e849d3a06e4c4176
parkjc <parkjc@hawaii.edu>
2016-03-11 14:24:38 -1000

    test search function

--- assets/js/main.js
+++ assets/js/main.js
@@ -123,4 +123,20 @@ $(document).ready(function(){
 			$(".tagjs").addClass('glyphicon-chevron-down').removeClass('glyphicon-chevron-up');
 		}
 	})
+
+	$('#bookmarkme').click(function() {
+    if (window.sidebar && window.sidebar.addPanel) { // Mozilla Firefox Bookmark
+        window.sidebar.addPanel(document.title,window.location.href,'');
+    } else if(window.external && ('AddFavorite' in window.external)) { // IE Favorite
+        window.external.AddFavorite(location.href,document.title); 
+    } else if(window.opera && window.print) { // Opera Hotlist
+        this.title=document.title;
+        return true;
+    } /*else { // webkit - safari/chrome
+        alert('Press ' + (navigator.userAgent.toLowerCase().indexOf('mac') != - 1 ? 'Command/Cmd' : 'CTRL') + ' + D to bookmark this page.');
+    }*/ else {
+    	alert('doesnt work');
+    	return false;
+    }
+	});
 });
