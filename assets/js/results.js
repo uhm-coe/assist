feb90865e07454faee3e208351fe43c6c01ebae5
Myles Enriquez <enri.myles@gmail.com>
2016-03-28 13:54:02 -1000

    Add tags in search results. Resolves #71.

--- assets/js/results.js
+++ assets/js/results.js
@@ -86,21 +86,26 @@ $(function(Query) {
 				tags = result.tags,
 				url = result.ref,
 				date = result.date;
+				tagLinks = "";
 
-				// TODO: List all tags relative to tag array size, in each category
-
+				// Go through all tags associated with post
+					for (var i = 0; i < tags.length; i++) {
+						if ( i == tags.length - 1) {
+							tagLinks += '<a href="/tag/'+ tags[i] +'">' + tags[i] + '</a>';
+						}
+						else {
+							tagLinks += '<a href="/tag/'+ tags[i] +'">' + tags[i] + ',</a>';
+						}
+					}
 
 				if (category === "How-to") {
-					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details how-to"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
-					for (var i = 0; i < tags.length; i++) {
-						$results.append('<a href="#">' + tags[i] + '</a>');
-					}
+					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details how-to"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div><div class="col-xs-12 col-md-7 pull-right"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>' + tagLinks + '</div></div></div></div>');
 				}
 				else if (category === 'Strategies') {
-					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details strategies"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
+					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details strategies"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div><div class="col-xs-12 col-md-7 pull-right"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>' + tagLinks + '</div></div></div></div>');
 				}
 				else {
-					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details troubleshooting"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div></div></div>');
+					$results.append('<div class="post-block"><div class="row"><div class="col-xs-12"><h2><a class="post-link" href="' + url + '">' + title + '</a></h2></div><div class="col-xs-12"><p class="post-blurb">' + blurb + '</p></div></div><div class="row post-details troubleshooting"><div class="col-xs-12 col-md-5"><span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span><a href="' + site + "/" + category.toLowerCase() + '/" title="View all posts published in ' + category + '">' + category + '</a></div><div class="col-xs-12 col-md-7 pull-right"><span class="glyphicon glyphicon-tags" aria-hidden="true"></span>' + tagLinks + '</div></div></div></div>');
 				}
 			});
 		}
